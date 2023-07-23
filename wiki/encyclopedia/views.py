0c538813d0d633484ec2e1b97d65f12326e8db23
Kevin Garcia Martin <kevingm@live.se>
2023-07-23 21:51:13 +0200

    Added markdown to HTML conversion

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+def md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
