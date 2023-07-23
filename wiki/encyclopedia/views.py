acc09db3eea44287a69d2be20c603d01519d75b9
Kevin Garcia Martin <kevingm@live.se>
2023-07-24 00:02:42 +0200

    Added a POST request to the search form allowing for encyclopedia entry search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -19,10 +19,31 @@ def index(request):
 def entry(request, title):
     content = md_to_html(title)
     if content == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found"
+        })
     else:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "content": content
         })
-
+        
+def search(request):
+    if request.method == "POST":
+        query = request.POST["q"]
+        content = md_to_html(query)
+        if content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": query,
+                "content": content
+            })
+        else:
+            search_result = []
+            entries = util.list_entries()
+            for entry in entries:
+                if query.lower() in entry.lower():
+                    search_result.append(entry)
+            return render(request, "encyclopedia/search_result.html", {
+                "query": query,
+                "search_result": search_result
+            })
