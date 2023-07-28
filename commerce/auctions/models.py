ef6dce1aeb99b755b0a0f6321d69c72e3230ccd5
Kevin Garcia Martin <kevingm@live.se>
2023-07-28 21:48:16 +0200

    Created admin, added 'Listing' model and render active listings on 'index' page

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    # image = models.ImageField()
+    item = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.item}: ${self.price}"
+    
+
+class Bid(models.Model):
+    number_of_bids = models.IntegerField()
+
+class Comment(models.Model):
+    pass
