4cdde9ab7670ebd5d59373b11eac734ef7a05324
Kevin Garcia Martin <kevingm@live.se>
2023-08-03 10:55:20 +0200

    Added 'Active Listings', 'Listings', 'Create Listing' and 'Watchlist'

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,23 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 
 class User(AbstractUser):
-    pass
-
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.username
+    
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.category_name
+    
 class Listing(models.Model):
-    # image = models.ImageField()
     item = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     description = models.CharField(max_length=500)
+    image = models.CharField(max_length=500, null=True)
+    lister = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="watchlist")
     timestamp = models.DateTimeField(auto_now_add=True)
+    isActive = models.BooleanField(default=True)
     
     def __str__(self):
         return f"{self.item}: ${self.price}"
-    
 
 class Bid(models.Model):
-    number_of_bids = models.IntegerField()
+    highest_bid = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True, related_name="highest_bid")
+    highest_bidder = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="highest_bidder")
+    number_of_bids = models.IntegerField(default=0)
 
 class Comment(models.Model):
     pass
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Listing, Bid, Comment
+from .models import User, Listing, Category, Bid, Comment
 
 
 def index(request):
     return render(request, "auctions/index.html", {
-        "listings": Listing.objects.all()
+        "listings": Listing.objects.filter(isActive=True)
     })
 
 
@@ -63,3 +63,63 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+def create_listing(request):
+    if request.method == "POST":
+        item = request.POST["title"]
+        description = request.POST["description"]
+        price = request.POST["price"]
+        image = request.POST["image"]
+        category = request.POST["category"]
+        category = Category.objects.get(category_name=category)
+        
+        new_listing = Listing(
+            item=item,
+            price=float(price),
+            description=description,
+            image=image,
+            lister=request.user,
+            category=category
+        )
+        new_listing.save()
+        return HttpResponseRedirect(reverse("index"))
+        
+    return render(request, "auctions/create.html", {
+        "categories": Category.objects.all()
+    })
+
+def listing(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    listing_in_watchlist = request.user in listing.watchlist.all()
+    return render(request, "auctions/listing.html", {
+        "listing": listing,
+        "listing_in_watchlist": listing_in_watchlist
+    })
+    
+def add_to_watchlist(request, listing_id):
+    if request.method == "POST":
+        listing = Listing.objects.get(pk=listing_id)
+        user = request.user
+        listing.watchlist.add(user)
+        return HttpResponseRedirect(reverse("listing", args=(listing_id, )))
+
+def remove_from_watchlist(request, listing_id):
+    if request.method == "POST":
+        listing = Listing.objects.get(pk=listing_id)
+        user = request.user
+        listing.watchlist.remove(user)
+        return HttpResponseRedirect(reverse("listing", args=(listing_id, )))
+    
+def watchlist(request):
+    user = request.user
+    watchlist = user.watchlist.all()
+    return render(request, "auctions/watchlist.html", {
+        "watchlist": watchlist
+    })
+    
+def bid(request, listing_id):
+    if request.method == "POST":
+        bid = request.POST["bid"]
+        listing = Listing.objects.get(ok=listing_id)
+        user = request.user
+        
