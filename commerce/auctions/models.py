3f486a4f165b5e00ba8cdecb1059ff88152e689c
Kevin Garcia Martin <kevingm@live.se>
2023-08-15 17:42:50 +0200

    Changed card styling and removed category form for 'Index' and 'Watchlist', added a default image to new listings

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -25,11 +25,12 @@ class Bid(models.Model):
         return f"{self.bidder} bid ${self.bid}"
     
 class Listing(models.Model):
+    
     item = models.CharField(max_length=64)
     listing_price = models.DecimalField(max_digits=20, decimal_places=2)
     highest_bid = models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="highest_bid")
     description = models.CharField(max_length=500)
-    image = models.CharField(max_length=500, null=True)
+    image = models.CharField(max_length=500)
     lister = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
     watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist")
@@ -39,6 +40,9 @@ class Listing(models.Model):
     
     def __str__(self):
         return self.item
+    
+    def imageDefault():
+        return "https://media.istockphoto.com/id/917901978/photo/gavel-on-auction-word.jpg?s=612x612&w=0&k=20&c=e5mnLUG2UEg6y8zfO1zc7Gi4Ed8PEEeV3eGeYOKxKBI="
 
 class Comment(models.Model):
     comment = models.CharField(max_length=200, null=True)
