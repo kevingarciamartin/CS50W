fe3fab1a39b8012d40b23dd4ad367b23d968a136
Kevin Garcia Martin <kevingm@live.se>
2024-02-02 16:34:28 +0100

    Add JS file

---
+++ network/static/network/app.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     // const posts = document.querySelectorAll('.post');
+//     // console.log(posts)
+//     // for (let i = 0; i < posts.length; i++) {
+//     //     console.log('HELLO' + i)
+//     // }
+
+//     document.querySelectorAll('.nav-link').style.backgroundColor = "red";
+// });
+
+function myFunction() {
+    alert("Hello from a static file!");
+}
